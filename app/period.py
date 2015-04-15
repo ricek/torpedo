44d9627b15338918e29e8fa66da0760dbf8706a8
nikitageorgiou <nikitageorgiou@gmail.com>
2015-04-15 11:22:25 -0400

    add covert timestamp to period function

---
+++ app/period.py
@@ -0,0 +1,47 @@
+from datetime import datetime
+from datetime import timedelta
+
+dict1 = [
+		'07:14',
+		'08:00',
+		'08:50',
+		'09:42',
+		'10:32',
+		'11:22',
+		'12:12',
+		'13:02',
+		'13:52',
+		'14:44',
+		'15:34',
+		'16:24'
+	]
+dict2 = [
+		'07:56',
+		'08:47',
+		'09:39',
+		'10:29',
+		'11:19',
+		'12:09',
+		'12:59',
+		'13:49',
+		'14:41',
+		'15:31',
+		'16:21',
+		'17:11'
+	]
+
+periodStart=[]
+periodEnd=[]
+for t in range(0,len(dict1)):
+   periodStart.append(datetime.strptime(dict1[t],'%H:%M'))
+   periodEnd.append(datetime.strptime(dict2[t],'%H:%M'))
+
+#print (periodStart)
+#print(periodEnd)
+
+def convertTS(timestamp):
+    time = timestamp.strftime('%H:%M')
+    rt = datetime.strptime(time,'%H:%M')
+    for x in range(0,len(periodStart)):
+    	if(periodStart[x]<=rt and rt<=periodEnd[x]):
+    		return x+1
