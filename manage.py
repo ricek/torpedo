ca9da44fd0c690bf552cf094dfa1025fd4623d22
Liang Gao <lianggao@outlook.com>
2015-04-15 16:42:22 -0400

    add database migrations with flask-migrate

--- manage.py
+++ manage.py
@@ -3,14 +3,17 @@ import os
 from app import create_app, db
 from app.models import Student, Course, Entry, User, Teacher
 from flask.ext.script import Manager, Shell
+from flask.ext.migrate import Migrate, MigrateCommand
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
+migrate = Migrate(app, db)
 
 def make_shell_context():
     return dict(app=app, db=db, Student=Student, Course=Course, Entry=Entry, User=User, Teacher=Teacher)
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command("db", MigrateCommand)
 
 @app.after_request
 def after_request(response):
