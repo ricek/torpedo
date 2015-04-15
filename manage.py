9678ddc3d8748cf4e69d9415b1e26f549323795a
Liang Gao <lianggao@outlook.com>
2015-04-15 19:35:23 -0400

    add the additional X-Requested-With header

--- manage.py
+++ manage.py
@@ -18,7 +18,7 @@ manager.add_command("db", MigrateCommand)
 @app.after_request
 def after_request(response):
     response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization')
     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
     return response
 
